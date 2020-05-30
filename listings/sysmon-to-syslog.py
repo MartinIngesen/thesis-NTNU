8aa7ec9c2ffbd976f13f4581bcc1b5b476fde9ea
Mrtn <martin@ingesen.no>
2020-05-30 15:10:24 +0200

    Updates from Overleaf

--- listings/sysmon-to-syslog.py
+++ listings/sysmon-to-syslog.py
@@ -5,21 +5,21 @@ def convertEvents(sysmon):
         if "Microsoft-Windows-Sysmon" in event:
             event = json.loads(event)
             
-            message = event["message"]
-            message = message.replace("\n", "  ")
+            m = event["message"]
+            m = m.replace("\n", "  ")
             
             if "computer_name" in event:
-                hostname = event["computer_name"]
+                h = event["computer_name"]
             elif "winlog" in event:
-                hostname = event["winlog"]["computer_name"]
+                h = event["winlog"]["computer_name"]
             else:
-                hostname = "NOHOSTNAME"
+                h = "NOHOSTNAME"
 
-            x = f"<14>Jan 01 00:00:00 {hostname} Microsoft-Windows-Sysmon[2092]: {message}"
+            x = f"<14>Jan 01 00:00:00 {h} Microsoft-Windows-Sysmon[2092]: {m}"
             print(x)
 
-with open('./apt3/caldera_attack_evals_round1_day1_2019-10-20201108.json', 'r') as sysmon:
+with open('./caldera_attack_evals_round1_day1_2019-10-20201108.json','r') as sysmon:
     convertEvents(sysmon)
 
-with open('./apt3/empire_apt3_2019-05-14223117.json', 'r') as sysmon:
+with open('./empire_apt3_2019-05-14223117.json','r') as sysmon:
     convertEvents(sysmon)
